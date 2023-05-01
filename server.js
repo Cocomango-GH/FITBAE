d87a8ff852e7b5668a19516af2fee6162ae904d1
cocomango-GH <111953271+Cocomango-GH@users.noreply.github.com>
2023-05-01 14:27:33 -0700

    server error

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'dist')));
-app.use(require('../fitbae/config/checkTokens'));
+app.use(require('./config/checkTokens'));
 
 app.use('/api/users', require('./routes/api/users'));
 //routes
